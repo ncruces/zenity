53fee94d3fe04e6d89f99d6d952cd9c1b5e7d4f7
Nuno Cruces <ncruces@users.noreply.github.com>
2020-12-11 19:06:41 +0000

    Fix macOS color dialog.

--- internal/zenutil/osa_generated.go
+++ internal/zenutil/osa_generated.go
@@ -11,7 +11,7 @@ var scripts = template.Must(template.New("").Funcs(template.FuncMap{"json": func
 	return string(b), err
 }}).Parse(`
 {{define "color" -}}
-tell application (path to frontmost application as text)
+tell application "SystemUIServer"
 activate
 set c to choose color default color { {{index . 0}},{{index . 1}},{{index . 2}} }
 "rgb(" & (item 1 of c) div 256 & "," & (item 2 of c) div 256 & "," & (item 3 of c) div 256 & ")"
--- internal/zenutil/osascripts/color.applescript
+++ internal/zenutil/osascripts/color.applescript
@@ -1,4 +1,4 @@
-tell application (path to frontmost application as text)
+tell application "SystemUIServer"
 	activate
 	set c to choose color default color { {{index . 0}},{{index . 1}},{{index . 2}} }
 	"rgb(" & (item 1 of c) div 256 & "," & (item 2 of c) div 256 & "," & (item 3 of c) div 256 & ")"
