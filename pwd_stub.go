0a67c8f6987d6e016e163b82cd82ce278d7d7af0
Nuno Cruces <ncruces@users.noreply.github.com>
2021-04-29 16:55:44 +0100

    Unsupported options.

--- pwd_stub.go
+++ pwd_stub.go
@@ -3,6 +3,9 @@
 package zenity
 
 func password(opts options) (string, string, error) {
+	if opts.username {
+		return "", "", ErrUnsupported
+	}
 	opts.hideText = true
 	str, err := entry("Password:", opts)
 	return "", str, err
