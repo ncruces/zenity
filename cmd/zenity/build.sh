357c0f2c0a37d08e97774965a703e39d440b2cee
Nuno Cruces <ncruces@users.noreply.github.com>
2021-05-22 11:55:52 +0100

    Build for Apple silicon.

--- cmd/zenity/build.sh
+++ cmd/zenity/build.sh
@@ -8,6 +8,11 @@ zip -9 zenity_win32.zip zenity.exe
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -trimpath &&
 zip -9 zenity_win64.zip zenity.exe
 
+rm resource.syso
+
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -trimpath &&
+zip -9 zenity_macos_arm.zip zenity
+
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -trimpath &&
 zip -9 zenity_macos.zip zenity
 
