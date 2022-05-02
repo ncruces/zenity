828102067b4dd5ab80bf28d7186b9491c6d691b9
Nuno Cruces <ncruces@users.noreply.github.com>
2022-05-02 16:05:42 +0100

    Trim binaries.

--- cmd/zenity/build.sh
+++ cmd/zenity/build.sh
@@ -5,16 +5,16 @@ echo 'package main; const tag = "'$TAG'"' > tag.go
 
 go run github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 
-GOOS=windows GOARCH=386   CGO_ENABLED=0 go build -trimpath &&
+GOOS=windows GOARCH=386   CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath &&
 zip -9 zenity_win32.zip zenity.exe
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath &&
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath &&
 zip -9 zenity_win64.zip zenity.exe
 
 rm resource.syso
 
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -o zenity_macos_x64 &&
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -o zenity_macos_arm &&
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath -o zenity_macos_x64 &&
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath -o zenity_macos_arm &&
 go run github.com/randall77/makefat zenity zenity_macos_x64 zenity_macos_arm &&
 zip -9 zenity_macos.zip zenity
 
