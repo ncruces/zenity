3a6fb3e70e3fdf400e42b28ac942c419ad6e8d57
Nuno Cruces <ncruces@users.noreply.github.com>
2022-05-05 13:21:00 +0100

    Custom entry dialog icons.

--- entry_darwin.go
+++ entry_darwin.go
@@ -12,7 +12,11 @@ func entry(text string, opts options) (string, error) {
 	data.Options.Answer = &opts.entryText
 	data.Options.Hidden = opts.hideText
 	data.Options.Timeout = zenutil.Timeout
-	data.Options.Icon = opts.icon.String()
+	if opts.customIcon != "" {
+		data.IconPath = opts.customIcon
+	} else {
+		data.Options.Icon = opts.icon.String()
+	}
 	data.SetButtons(getButtons(true, true, opts))
 
 	out, err := zenutil.Run(opts.ctx, "dialog", data)
