623da5c26a99e082722ea1ab8de2d20baddbff60
hfmrow <45952208+hfmrow@users.noreply.github.com>
2024-08-13 17:27:18 +0200

    On Windows, file dialogs display extension filters followed by a ';'
    - A simple fix for file dialog handling on Windows regarding extension filters.

--- file_windows.go
+++ file_windows.go
@@ -517,9 +517,11 @@ func initFileTypes(filters FileFilters) (int, *win.COMDLG_FILTERSPEC) {
 			continue
 		}
 		var spec []uint16
-		for _, p := range f.Patterns {
+		for i, p := range f.Patterns {
 			spec = append(spec, syscall.StringToUTF16(p)...)
-			spec[len(spec)-1] = ';'
+			if i != len(f.Patterns)-1 {
+				spec[len(spec)-1] = ';'
+			}
 		}
 		res = append(res, win.COMDLG_FILTERSPEC{
 			Name: syscall.StringToUTF16Ptr(f.Name),
